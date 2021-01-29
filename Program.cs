6657b82558e4f36f19ce57953b3f21646cefca6a
Majd Maghamez <majdmaghamez@gmail.com>
2021-01-29 00:55:50 -0500

    committing project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace stock_insight_file_mover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string source = ConfigurationManager.AppSettings["source"];
+                string[] files = Directory.GetFiles(source);
+
+                foreach(string file in files)
+                {
+                    string filename = Path.GetFileName(file);
+                    string destination = Path.Combine( ConfigurationManager.AppSettings["destination"], filename );
+                    File.Copy(file, destination, true);
+                }
+
+                DirectoryInfo directory = new DirectoryInfo(source);
+
+                foreach ( FileInfo file in directory.GetFiles())
+                {
+                    file.Delete();
+                }            
+
+            }
+            catch(IOException Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+        }
+    }
+}
